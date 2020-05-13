909a2b153513ea10c36e28a7049a3cb11182cf1b
Francesco <francescodtroiano@gmail.com>
2020-05-13 13:31:20 -0500

    First two tests - Empty list exists, and adding to it

--- GenericLibrary.Tests/UnitTest1.cs
+++ GenericLibrary.Tests/UnitTest1.cs
@@ -6,9 +6,23 @@ namespace GenericLibrary.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void InitialListEmpty()
         {
+            MyGenericList<string> list = new MyGenericList<string>();
 
+            Assert.Equal(object, list.Count);
+
+        }
+
+        [Fact]
+        public void CanAddEmptyList()
+        {
+            MyGenericList<string> list = new MyGenericList<string>();
+
+            list.Add(2);
+
+            Assert.Equal(1, list.Count);
+            Assert.Equal(2, list[0]);
         }
     }
 }
