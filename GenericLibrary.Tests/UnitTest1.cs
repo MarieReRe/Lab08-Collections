e4331281d8f78628bf985539cc5cc626e3611ae8
Francesco <francescodtroiano@gmail.com>
2020-05-13 14:17:46 -0500

    capacity testing

--- GenericLibrary.Tests/UnitTest1.cs
+++ GenericLibrary.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace GenericLibrary.Tests
@@ -27,6 +28,27 @@ namespace GenericLibrary.Tests
             Assert.Equal(4, list.TotalOfBooks);
             Assert.Equal(2, list[0]);
         }
+
+        [Fact]
+        public void CanAddMoreThanCapacity()
+        {
+            const int Capacity = 1;
+            GenericLibrary<int> list = new GenericLibrary<int>(Capacity);
+            list.AddBook(2);
+            list.AddBook(4);
+            list.AddBook(10);
+
+
+            Assert.Equal(3, list.TotalOfBooks);
+            Assert.Equal(2, list[0]);
+
+            list.AddBook(1);
+
+            Assert.Equal(4, list.TotalOfBooks);
+            Assert.Equal(1, list[3]);
+        }
+
+
 
         
     }
